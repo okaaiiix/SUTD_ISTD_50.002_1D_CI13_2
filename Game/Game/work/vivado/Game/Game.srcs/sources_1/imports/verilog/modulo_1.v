a179eeff368405ad9a5f01186ae6bb17bb7d70c5
namoikonk <naomikongvega@gmail.com>
2020-12-02 15:20:59 +0800

    blah blah blah

---
+++ Game/Game/work/vivado/Game/Game.srcs/sources_1/imports/verilog/modulo_1.v
@@ -0,0 +1,21 @@
+module modulo_1 (
+    input a,  
+    input b,  
+    output out
+  );
+
+  /* Combinational Logic */
+  always @* begin
+    out = a % b;
+  end
+  
+  /* Sequential Logic */
+  always @(posedge clk) begin
+    if (rst) begin
+      // Add flip-flop reset values here
+    end else begin
+      // Add flip-flop q <= d statements here
+    end
+  end
+  
+endmodule
